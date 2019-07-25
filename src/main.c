b0ce13e736732ea6f30b3c62949178012c9d8d72
tolfreedom <tolfreetroy@gmail.com>
2019-07-24 18:10:24 -0700

    second commit

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 
 int main( int argc, char * argv[] ) {
 	
+	//initialization
 	if(ww_window_create(argc, argv, "Pixarray", 1024, 576)) {
 		printf("Closing..\n");
 		return 1;
@@ -18,23 +19,55 @@ int main( int argc, char * argv[] ) {
 	
 	//primary loop
 	while(!ww_window_received_quit_event()) {
-		
-	
-		
+				
 		//update events
 		ww_window_update_events();	
-		
-		if( keystate.esc == 1){
+	
+		//quit
+		if (keystate.esc == 1){
 			printf ("ESC pressed\n") ;
+			ww_window_send_quit_event() ; 
+		}
+		
+		//set animation
+		if (keystate.w == 1 && face->active_animation == 0){
+			face->active_animation = 1;
+		}
+		else if (keystate.w == 1){
+			face->active_animation = 0;
+		}
+		
+		// move x
+		face->pad_x = face->pad_x +3;
+		if (face->pad_x > 1000){
+			face->pad_x = 0;
+		}
+		if (face->pad_x > 1000){
+			face->pad_x = face->pad_x -3 ;
+		}
+		
+		//move y
+		face->pad_y = face->pad_y +3;
+		if (face->pad_y > 500){
+			face->pad_y = 0;
 		}
 		
 		ww_draw_sprite(face) ; 
 		
-		
-		ww_window_update_buffer();
+		//draw screen
+		ww_window_update_buffer() ; 
 	}
 	
-	//Cleanup and exit
-	ww_window_destroy();
-	return 0;
-}
+		ww_free_sprite(face) ; 
+		
+		//clean up and exit
+		ww_window_destroy() ; 
+		return 0;
+	}
+	
+		
+		
+	
+		
+	
+
